61a92f6f9bbe838509706ce98c2c0d1e9d925777
Ben Tranter <ben@bentranter.io>
2023-01-10 18:45:11 -0500

    seatbelt: add "route" router method
    Adds Chi's `route` to the Seatbelt application's router.
    
    This allows sub-routes to be created, as follows:
    
    ```go
    app := app.New()
    app.Route("/admin", func(app *App) {
        app.Get("/home", func(c *seatbelt.Context) error {
            return c.String(200, "ok")
        })
    })
    app.Start()
    ```
    
    This will return 200 with the body "ok" when a request is sent to
    `/admin/home`.

--- seatbelt_test.go
+++ seatbelt_test.go
@@ -1,6 +1,9 @@
 package seatbelt
 
 import (
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
@@ -17,6 +20,56 @@ func TestOptions(t *testing.T) {
 		}
 		if data == nil {
 			t.Fatal("file is empty")
+		}
+	})
+}
+
+func TestSubRouter(t *testing.T) {
+	app := New()
+
+	app.Get("/", func(c *Context) error {
+		return c.String(200, "home")
+	})
+	app.Route("/admin", func(app *App) {
+		app.Get("/home", func(c *Context) error {
+			return c.String(200, "ok")
+		})
+	})
+
+	srv := httptest.NewServer(app)
+	defer srv.Close()
+
+	t.Run("GET /", func(t *testing.T) {
+		resp, err := http.Get(srv.URL + "/")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if string(data) != "home" {
+			t.Fatalf("expected home but got %s", data)
+		}
+	})
+
+	t.Run("GET /admin/home", func(t *testing.T) {
+		resp, err := http.Get(srv.URL + "/admin/home")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if string(data) != "ok" {
+			t.Fatalf("expected ok but got %s", data)
 		}
 	})
 }
