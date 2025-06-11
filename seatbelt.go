710d44b4c6822e33a46ee4169bc460d9310afbfd
Ben Tranter <ben@bentranter.io>
2025-06-11 18:44:23 -0400

    release: v0.4.1

--- seatbelt.go
+++ seatbelt.go
@@ -24,7 +24,7 @@ import (
 )
 
 // Version is the version of the Seatbelt package.
-const Version = "v0.4.0"
+const Version = "v0.4.1"
 
 // ChiPathParamFunc extracts path parameters from the given HTTP request using
 // the github.com/go-chi/chi router.
